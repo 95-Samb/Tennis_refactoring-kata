4dc5c1a2804f983914e5af89a7a53b0d8e7933b9
95-Samb <samuelbrown201195@hotmail.co.uk>
2019-07-16 17:19:06 +0100

    changes made to 1st game

--- ruby/tennis.rb
+++ ruby/tennis.rb
@@ -2,12 +2,12 @@
 class TennisGame1
 
   def initialize(player1Name, player2Name)
-    @player1Name = player1Name
-    @player2Name = player2Name
+    #@player1Name = player1Name
+    #@player2Name = player2Name
     @p1points = 0
     @p2points = 0
   end
-        
+
   def won_point(playerName)
     if playerName == "player1"
       @p1points += 1
@@ -15,42 +15,24 @@ class TennisGame1
       @p2points += 1
     end
   end
-  
+
   def score
-    result = ""
-    tempScore=0
-    if (@p1points==@p2points)
-      result = {
-          0 => "Love-All",
-          1 => "Fifteen-All",
-          2 => "Thirty-All",
-      }.fetch(@p1points, "Deuce")
-    elsif (@p1points>=4 or @p2points>=4)
-      minusResult = @p1points-@p2points
-      if (minusResult==1)
-        result ="Advantage player1"
-      elsif (minusResult ==-1)
-        result ="Advantage player2"
-      elsif (minusResult>=2)
-        result = "Win for player1"
-      else
-        result ="Win for player2"
-      end
-    else
-      (1...3).each do |i|
-        if (i==1)
-          tempScore = @p1points
-        else
-          result+="-"
-          tempScore = @p2points
-        end
-        result += {
-            0 => "Love",
+    score_hash = {0 => "Love",
             1 => "Fifteen",
             2 => "Thirty",
-            3 => "Forty",
-        }[tempScore]
+            3 => "Forty"}
+    if (@p1points==@p2points )
+      same_score_hash = score_hash.reject{|k,v| k == 3}.transform_values{|v| v += "-All"}
+      result = same_score_hash.fetch(@p1points, "Deuce")
+    elsif (@p1points>=4 or @p2points>=4)
+      minusResult = @p1points-@p2points
+      if (minusResult.abs == 1)
+        result ="Advantage #{@p1points>@p2points ? "player1" : "player2"}"
+      else
+        result ="Win for #{@p1points>@p2points ? "player1" : "player2"}"
       end
+    else
+      result =(score_hash[@p1points] + "-" + score_hash[@p2points])
     end
     result
   end
@@ -63,7 +45,7 @@ class TennisGame2
     @p1points = 0
     @p2points = 0
   end
-      
+
   def won_point(playerName)
     if playerName == @player1Name
       p1Score()
@@ -89,7 +71,7 @@ class TennisGame2
     if (@p1points==@p2points and @p1points>2)
         result = "Deuce"
     end
-    
+
     p1res = ""
     p2res = ""
     if (@p1points > 0 and @p2points==0)
@@ -115,11 +97,11 @@ class TennisGame2
       if (@p2points==3)
         p2res = "Forty"
       end
-      
+
       p1res = "Love"
       result = p1res + "-" + p2res
     end
-    
+
     if (@p1points>@p2points and @p1points < 4)
       if (@p1points==2)
         p1res="Thirty"
@@ -180,7 +162,7 @@ class TennisGame2
   def p1Score
     @p1points +=1
   end
-  
+
   def p2Score
     @p2points +=1
   end
@@ -193,7 +175,7 @@ class TennisGame3
     @p1 = 0
     @p2 = 0
   end
-      
+
   def won_point(n)
     if n == @p1N
         @p1 += 1
@@ -201,7 +183,7 @@ class TennisGame3
         @p2 += 1
     end
   end
-  
+
   def score
     if (@p1 < 4 and @p2 < 4) and (@p1 + @p2 < 6)
       p = ["Love", "Fifteen", "Thirty", "Forty"]
